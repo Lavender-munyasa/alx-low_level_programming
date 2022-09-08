2ee9b8318050d8a46e2dc50668e7e243c8295a15
Lavender Munyasa <lavendermunyasa23@gmail.com>
2022-09-08 05:54:56 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,9 @@
 #!/bin/bash
-#include <stdio.h>
-
-
-
+#include <studio.h>
 /**
+ * main - entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-int main(void){ puts("Programming is like building a multilingual puzzle\n"); return (0); }
+ * Return : 0 after printing the function
+ */
+int main(void)
+{puts("Programming is like building a multilingual puzzle\n"); return (0); }
