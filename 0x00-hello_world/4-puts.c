0419822e146b0f5cf0191278dd3a7da10f342d70
Lavender Munyasa <lavendermunyasa23@gmail.com>
2022-09-08 05:47:23 -0700

    trials on the c task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,16 @@
 #!/bin/bash
-include <studio.h> int main(void) {puts("Programming is like building a multilingual puzzle\n"); return (0); }
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void){ puts("Programming is like building a multilingual puzzle\n"); return (0); }
