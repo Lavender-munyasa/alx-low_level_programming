1f0e69af8308093ff7fdf96796470b051feb75a0
Lavender Munyasa <lavendermunyasa23@gmail.com>
2022-09-08 08:41:01 -0700

    fixing betty errors

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,8 @@
  * main - C program that prints the size of various types on the computer it is compiled and run on
  *
  * description: C program that prints the size of various types on the computer it is compiled and run on
- */
+ * return: 0
+ * */
 int main(void)
 {
 	char c;
@@ -12,10 +13,13 @@ int main(void)
 	long li;
 	long long lli;
 	float f;
+
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a long int: %ld byte(s)\n", sizeof
+	(li));
+	printf("Size of a long long int: %ld byte(s)\n",
+	sizeof(lli));
 	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
