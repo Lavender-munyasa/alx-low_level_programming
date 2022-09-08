8e07321282723300acb17839d3731d831204a1f4
Lavender Munyasa <lavendermunyasa23@gmail.com>
2022-09-08 06:04:29 -0700

    4 puts c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <studio.h>
+
 /**
  * main - entry point
  *
  * Return : 0 after printing the function
  */
 int main(void)
-{puts("Programming is like building a multilingual puzzle\n");
+{puts("\"Programming is like building a multilingual puzzle");
        	return (0); }
