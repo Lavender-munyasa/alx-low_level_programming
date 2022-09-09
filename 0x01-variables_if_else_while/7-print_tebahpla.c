3a0cd015b63fbf764abb642100c30f52e986f942
Lavender Munyasa <lavendermunyasa23@gmail.com>
2022-09-09 14:07:38 -0700

    7-reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print if the number is postive, zero, or negative
+ *
+ * Description: using the main function
+ *
+ * this program is for reverse alphabets
+ * Return: 0
+ */
+
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; c--)
+{
+	putchar(c);
+}
+putchar('\n');
+return (0);
+}
