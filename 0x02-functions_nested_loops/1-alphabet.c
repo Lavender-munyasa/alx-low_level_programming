ed5b20ce66da0a60beccc540fc1fea1e8502dcec
Lavender Munyasa <lavendermunyasa23@gmail.com>
2022-09-12 09:01:11 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - main code block
+ *
+ * Description - program that prints the alphabet in lower case
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <='z';c++)
+		_putchar(c);
+	_putchar('/n')
+}		
