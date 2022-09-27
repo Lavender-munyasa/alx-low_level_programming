4525d47bfc5a7b5c5e0ff0131e64ff7bc955601a
Lavender-munyasa <lavendermunyasa23@gmail.com>
2022-09-27 09:47:18 -0700

    all mandatory tasks

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memcpy - coppies memory data
+ *
+ * @n: bytes to be coppied
+ *
+ * @src: memory area
+ *
+ * @dest: memory area
+ * Return: pointer to 'dest'
+ *
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
