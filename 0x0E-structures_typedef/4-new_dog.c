22d14c785cae65112353f2d04b7c4d6070dc2085
Lavender-munyasa <lavendermunyasa23@gmail.com>
2022-10-11 14:53:49 -0700

    4/5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,75 +16,39 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+unsigned int i, namel, ownerl;
+dog_t *dog;
 
-	unsigned int i, namel, ownerl;
+if (name == NULL || owner == NULL)
+return (NULL);
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+return (NULL);
+for (namel = 0; name[namel]; namel++)
+;
+namel++;
+dog->name = malloc(namel * (sizeof(char)));
+if (dog->name == NULL)
+{
+free(dog);
+return (NULL);
+}
+for (i = 0; i < namel; i++)
+dog->name[i] = name[i];
 
-	dog_t *dog;
+dog->age = age;
 
-
-
-	if (name == NULL || owner == NULL)
-
-		return (NULL);
-
-	dog = malloc(sizeof(dog_t));
-
-	if (dog == NULL)
-
-		return (NULL);
-
-	for (namel = 0; name[namel]; namel++)
-
-		;
-
-	namel++;
-
-	dog->name = malloc(namel * (sizeof(char)));
-
-	if (dog->name == NULL)
-
-	{
-
-		free(dog);
-
-		return (NULL);
-
-	}
-
-	for (i = 0; i < namel; i++)
-
-		dog->name[i] = name[i];
-
-
-
-	dog->age = age;
-
-
-
-	for (ownerl = 0; owner[ownerl]; ownerl++)
-
-		;
-
-	ownerl++;
-
-	dog->owner = malloc(ownerl * (sizeof(char)));
-
-	if (dog->owner == NULL)
-
-	{
-
-		free(dog->name);
-
-		free(dog);
-
-		return (NULL);
-
-	}
-
-	for (i = 0; i < ownerl; i++)
-
-		dog->owner[i] = owner[i];
-
-	return (dog);
-
+for (ownerl = 0; owner[ownerl]; ownerl++)
+;
+ownerl++;
+dog->owner = malloc(ownerl * (sizeof(char)));
+if (dog->owner == NULL)
+{
+free(dog->name);
+free(dog);
+return (NULL);
+}
+for (i = 0; i < ownerl; i++)
+dog->owner[i] = owner[i];
+return (dog);
 }
