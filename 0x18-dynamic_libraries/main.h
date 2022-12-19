322a0c75cde0faabcc17fd0b9010a44844bdd062
Lavender-munyasa <lavendermunyasa23@gmail.com>
2022-12-19 07:01:07 -0800

    task 1

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,9 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -26,4 +25,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif 
